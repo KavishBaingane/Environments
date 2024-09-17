a525361cc1dda32fd2d95320bb9434fa909f6e8e
KavishBaingane <104kbaingane@gmail.com>
2024-09-17 13:46:39 +0530

    master:Environment in launch settings

--- Environments/Controllers/HomeController.cs
+++ Environments/Controllers/HomeController.cs
@@ -4,8 +4,14 @@ namespace Environments.Controllers
 {
     public class HomeController : Controller
     {
-        Route("/")
+        [Route("/some-route")]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/some-route")]
+        public IActionResult Other()
         {
             return View();
         }
